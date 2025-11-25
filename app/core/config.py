fd706039057449099e38e30ed645d636ec7dec7b
Mohammad Dakhilitarghi <mohamaddakhiliuad@gmail.com>
2025-11-24 22:27:42 -0500

    Refactor Settings class to use SettingsConfigDict
    Updated to Pydantic v2

--- app/core/config.py
+++ app/core/config.py
@@ -1,11 +1,13 @@
-﻿from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    app_name: str = "CleanARC"
-    database_url: str = 
+    DATABASE_URL: str
+    OPENAI_API_KEY: str
 
-    class Config:
-        env_file = ".env"
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore",
+    )
 
 settings = Settings()
-***read
